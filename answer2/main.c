7f16c44100c15c9b0aaa9521bf41491dcf06d6d7
EmreYigitOzturk <118299119+EmreYigitOzturk@users.noreply.github.com>
2022-12-06 15:56:47 +0300

    Add files via upload

---
+++ answer2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int sum,r,p=3.14;
+    printf("yarýçarý giriniz\n");
+    scanf("%d",&r);
+    sum=p*r^2;
+    printf("alan= %d",sum);
+
+
+
+
+
+    return 0;
+}
